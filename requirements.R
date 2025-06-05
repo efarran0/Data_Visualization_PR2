222e4a0b67d6dd0a6ef7f4c3823faebf5a6482d3
efarran0 <165324744+efarran0@users.noreply.github.com>
2025-06-05 19:53:30 +0200

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -1,2 +1,9 @@
-packages <- c("readr", "mapSpain", "shiny", "dplyr", "plotly", "leaflet", "htmltools", "shinythemes", "shinyWidgets", "shinyjs", "sf")
-install.packages(packages)
+packages <- c(
+  "readr", "mapSpain", "shiny", "dplyr", "plotly", "leaflet",
+  "htmltools", "shinythemes", "shinyWidgets", "shinyjs", "sf"
+)
+
+new_packages <- setdiff(packages, rownames(installed.packages()))
+if (length(new_packages)) install.packages(new_packages)
+
+lapply(packages, library, character.only = TRUE)
